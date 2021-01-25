f83bbf25ec9cffecf895800414066bd25c8f0190
bschorchit <b.schorchit@gmail.com>
2021-01-25 19:08:01 -0300

    Updates

---
+++ precache-manifest.0cff0e4b41d52d207781de157afa6541.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebdf91fbccf8e94cad8f490422319ba",
+    "url": "/consensys-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "892f60007d6483d7b75d",
+    "url": "/consensys-bootcamp-final-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ae617bfec15773c7254a",
+    "url": "/consensys-bootcamp-final-project/static/js/2.0e2eb6b3.chunk.js"
+  },
+  {
+    "revision": "892f60007d6483d7b75d",
+    "url": "/consensys-bootcamp-final-project/static/js/main.800ad2b4.chunk.js"
+  },
+  {
+    "revision": "10b0383b833af8aafc43",
+    "url": "/consensys-bootcamp-final-project/static/js/runtime-main.5debd6dc.js"
+  }
+]);
